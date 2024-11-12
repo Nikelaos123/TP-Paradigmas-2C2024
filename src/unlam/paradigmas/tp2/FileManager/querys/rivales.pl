8dd8f3d47f96f4303cf423d7feb7052e88b01851
Nicolas <nicodjan200126@gmail.com>
2024-11-12 19:47:27 -0300

    idem

--- src/unlam/paradigmas/tp2/FileManager/querys/rivales.pl
+++ src/unlam/paradigmas/tp2/FileManager/querys/rivales.pl
@@ -1,5 +1,5 @@
-rival(aquamaris, 0, 0).
-rival(bernard, 0, 0).
-rival(lucas, 0, 0).
-rival(hornet, 0, 0).
-rival(luna, 30, 0).
+rival(beltharoth, 0, 0).
+rival(daemon, 0, 0).
+rival(ravok, 0, 0).
+rival(astarot, 0, 0).
+rival(vorgar, 40, 0).
--- src/unlam/paradigmas/tp2/hechizos/Hechizo.java
+++ src/unlam/paradigmas/tp2/hechizos/Hechizo.java
@@ -3,19 +3,13 @@ package unlam.paradigmas.tp2.hechizos;
 import unlam.paradigmas.tp2.personajes.Personaje;
 
 public interface Hechizo {
-	
+
 	public void ejecutar(Personaje objetivo);
-	
+
 	public String getNombre();
+
 	public double getPoder();
+
 	public double getCosto();
-	// RARO -> para nada
-	/*
-	public Hechizo getCosto();
-	public Hechizo getPoder();
-	public Hechizo getNombre();
-	*/
-	
-	
 
 }
